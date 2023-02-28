c48fc6f620e4e8779096768fed039d2168ef9048
Gilles Covens <gilles.covens@gmail.com>
2023-02-28 13:54:16 +0100

    help

---
+++ Hackaton/Hackaton/Controllers/VivesHackathon.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Hackaton.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+   
+    public class VivesHackathon : ControllerBase
+    {
+        [HttpGet(Name = "getStudent")]
+
+        public String Get()
+        {
+            return "Gilles";
+        }
+    }
+}
