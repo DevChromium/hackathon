9b940b1058e3c56dcebebb8a58e1ac2bf65a89d6
Gilles Covens <gilles.covens@gmail.com>
2023-02-28 12:45:36 +0100

    api

---
+++ Hackaton/Hackaton/WeatherForecast.cs
@@ -0,0 +1,11 @@
+namespace Hackaton
+{
+    public class WeatherForecast
+    {
+        public string? Name { get; set; }
+
+        public string? Klas { get; set; }
+
+        public string? Foto { get; set; }
+    }
+}
